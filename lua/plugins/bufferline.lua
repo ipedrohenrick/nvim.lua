6f3ef7c55bcda70eaed927601b4d21450ae25f48
Pedro Henrick <pedrohenrickqa@gmail.com>
2023-01-25 22:14:30 -0300

    fix(bufferline): buferline title in nvimtree

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -10,5 +10,13 @@ bufferline.setup({
         show_buffer_close_icons = false,
         show_close_icon = false,
         color_icons = true,
+        offsets = {
+          {
+            filetype = "NvimTree",
+            text = "File Explorer",
+            highlight = "Directory",
+            separator = true,
+          },
+        },
     }
 })
