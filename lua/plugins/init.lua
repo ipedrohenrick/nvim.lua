496665675345e160f166f2bdd64f73b7e36e4c94
Pedro Henrick <pedrohenrickqa@gmail.com>
2023-04-09 13:02:29 -0300

    feat: add autopairs

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -68,6 +68,21 @@ if lazy_status then
         require('Comment').setup()
       end
     },
+    {
+      'windwp/nvim-autopairs',
+      event = 'InsertEnter',
+      opts = {
+        check_ts = true,
+        ts_config = {
+          lua = {'string'},
+          javascript = {'template_string'},
+          java = false,
+        }
+      },
+      config = function(_, opts)
+        require('nvim-autopairs').setup(opts)
+      end
+    },
 
     -- lang configs
     {
