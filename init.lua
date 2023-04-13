c333bc3475de3c4f11bc2aa0414c1b1856b2f77e
Pedro Henrick <pedrohenrickqa@gmail.com>
2023-04-12 22:12:37 -0300

    feat: add post bootstrap install

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ local lazypath = vim.fn.stdpath('data') .. 'lazy/lazy.nvim'
 
 if not vim.loop.fs_stat(lazypath) then
   require('core.bootstrap').lazy(lazypath)
+  require('core.bootstrap').post_bootstrap()
 end
 
 vim.opt.rtp:prepend(lazypath)
--- lua/core/bootstrap.lua
+++ lua/core/bootstrap.lua
@@ -12,7 +12,23 @@ M.lazy = function(install_path)
   vim.opt.rtp:prepend(install_path)
 
   require 'plugins'
-  vim.api.nvim_buf_delete(0, { force = true })
+end
+
+M.post_bootstrap = function()
+  local api = vim.api
+
+  api.nvim_buf_delete(0, { force = true })
+
+  vim.schedule(function()
+    vim.cmd "MasonInstallAll"
+
+    -- Keep track of which mason pkgs get installed
+    local packages = table.concat(vim.g.mason_binaries_list, " ")
+
+    require("mason-registry"):on("package:install:success", function(pkg)
+      packages = string.gsub(packages, pkg.name:gsub("%-", "%%-"), "") -- rm package name
+    end)
+  end)
 end
 
 return M
