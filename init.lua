fdff706c2049a9a3dce91d77f1fb628d76ce9047
Pedro Henrick <pedrohenrickqa@gmail.com>
2023-09-28 11:26:51 -0300

    chore: remove bootstrap script

--- init.lua
+++ init.lua
@@ -1,10 +1,15 @@
-local lazypath = vim.fn.stdpath('data') .. 'lazy/lazy.nvim'
-
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  require('core.bootstrap').lazy(lazypath)
-  require('core.bootstrap').post_bootstrap()
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
-
 vim.opt.rtp:prepend(lazypath)
+
 require 'core'
 require 'plugins'
