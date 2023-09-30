c03f4ebc20b0f08675a2d328d08f93f94833f3e4
Pedro Henrick <pedrohenrickqa@gmail.com>
2023-09-30 00:12:34 -0300

    chore: update default installed lsps

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -178,6 +178,8 @@ if lazy_status then
         require('mason').setup()
         require('mason-lspconfig').setup({
           ensure_installed = {
+            'cssls',
+            'html',
             'lua_ls',
             'pyright',
           },
