ccbde820344f7fdc6287e9eb6b0609462eb6ba6f
Pedro Henrick <pedrohenrickqa@gmail.com>
2025-07-09 14:54:23 -0300

    chore: remove comments

--- init.lua
+++ init.lua
@@ -2,17 +2,3 @@ require('config.options')
 require('config.keymaps')
 require('config.lazy')
 require('config.autocmds')
-
-
--- https://github.com/numToStr/Comment.nvim
--- https://github.com/echasnovski/mini.pairs
--- https://github.com/lewis6991/gitsigns.nvim
-
--- https://github.com/mason-org/mason.nvim
--- https://github.com/mason-org/mason-lspconfig.nvim
--- https://github.com/neovim/nvim-lspconfig
--- https://github.com/Saghen/blink.cmp
-
--- https://github.com/L3MON4D3/LuaSnip ?
--- https://github.com/folke/trouble.nvim ?
--- https://github.com/nvimdev/lspsaga.nvim ?
